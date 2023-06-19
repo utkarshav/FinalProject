064b7f2f3e1f6e2a1afd435fe9f1b6014249e598
utkarshav <Utkarsha.20vinkare@gmail.com>
2023-06-19 18:19:32 +0530

    commit 1

---
+++ Node JS/ass01.js.js
@@ -0,0 +1,6 @@
+//Node js
+console.log("Hello from Node");
+console.log("path of file"+__filename);
+console.log("path of current directory"+__dirname);
+console.log("process current working directory"+process.pid);
+console.log("process id is:"+process.cwd)
