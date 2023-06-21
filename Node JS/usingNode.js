59020d7c45b0d72d534c2c3e3c6adacbd0a6547e
riitwik <77431797+riitwik@users.noreply.github.com>
2023-06-21 21:04:25 +0530

    Delete Node JS directory

--- Node JS/usingNode.js
+++
@@ -1,6 +0,0 @@
-var index = require('./ass02.js');
-
-
-console.log("Is the number prime:"+index.isPrime(3));
-console.log("factorial : "+index.calFact(6));
-console.log("is perfect : "+index.isPerfect(28));
