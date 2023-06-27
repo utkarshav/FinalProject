ca3eafa3bd812da43a2a2f076ba043b55005a972
riitwik <riitwik@gmail.com>
2023-06-27 22:18:09 +0530

    Pushing front end files

---
+++ firstreactapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import StudentForm_078 from './Components/StudentForm_078';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <StudentForm_078/>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
